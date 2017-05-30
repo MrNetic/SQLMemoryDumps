197e0ae3084aeb6dea8da4d901a79d2df0170c25
Paulo Condeça <paulo@citen.net>
2017-05-30 13:46:27 +0100

    Transaction Log Analysis

--- TransactionLogAnalysis/DetailedTransactionLogInformation.sql
+++ TransactionLogAnalysis/DetailedTransactionLogInformation.sql
@@ -1,6 +1,8 @@
 
 -- Example 01 
-SELECT * FROM sys.dm_db_log_stats
+SELECT @@SERVERNAME,db.database_id,db.name,delayed_durability_desc,ls.* FROM sys.databases db
+CROSS APPLY  sys.dm_db_log_stats(db.database_id) ls
+
 
 -- Example 02
 
